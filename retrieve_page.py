7f9619809dd4e7643704d3f31ad675f6357539ce
nougatine <106869074+nougat-the-cat@users.noreply.github.com>
2022-12-19 02:45:38 +0100

    Update retrieve_page.py

--- retrieve_page.py
+++ retrieve_page.py
@@ -7,6 +7,9 @@ import winsound
 
 newsletter = "https://forocoches.substack.com/"
 
+def unhyphen(string):
+  return string.replace("-", "")
+
 def init():
     chromedriver_autoinstaller.install()
     driver = webdriver.Chrome()
@@ -31,11 +34,25 @@ def reload():
             instrucciones = párrafos[1].text
             códigos = [párrafos[2].text, párrafos[3].text, párrafos[4].text]
             print(instrucciones)
-            print(códigos)
-            notes = {'C': 1635, 'E': 2060, 'F': 2183, 'G': 2450, 'A': 2750, ' ': 37}
-            melody = 'EAGC CGAE AFGC CGAF' 
-            for note in melody:
-                winsound.PlaySound(notes[note], 700)
+            for código in códigos:
+                clean_string = unhyphen(código)
+                print(clean_string)
+            
+            note_freqs = {
+                "C4": 261.63, "C#4": 277.2, "D4": 293.66, "Eb4": 311.1, "E4": 329.63, "F4": 349.23, 
+                "F#4": 370, "G4": 392.00, "G#4": 415.3, "A4": 440.00, "Bb4": 466.2, "B4": 493.88, " ": 37,
+            }
+
+            melody = [
+                ("F4", 125), ("A4", 125), ("G4", 125), ("C4", 125), (" ", 125), 
+                ("A4", 125), ("F4", 125), ("G4", 125), ("C4", 125), (" ", 125), 
+                ("C4", 250), ("G4", 125), ("A4", 125), ("F4", 125), (" ", 125), 
+                ("F4", 125), ("G4", 125), ("A4", 125), ("F4", 125), (" ", 125), 
+            ]
+
+            for note, duration in melody:
+                freq = note_freqs[note]
+                winsound.Beep(int(freq), duration*6)
             break
         time.sleep(15)
 
