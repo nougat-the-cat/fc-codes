49537930287266dd81a63862ff0e88d5a4f5ec89
nougatine <106869074+nougat-the-cat@users.noreply.github.com>
2023-01-09 11:33:42 +0100

    Update retrieve_page.py

--- retrieve_page.py
+++ retrieve_page.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 import time
 import winsound
+from methods import *
 
 
 TOKEN = "[YOUR TOKEN]"
@@ -23,65 +24,6 @@ def init():
     return driver
 
 driver = init()
-
-
-def unhyphen(string):
-    clean = string.replace("-", "").replace(".", "").replace("_", "").replace(" ", "")
-    return clean
-
-def swapcase(codes):
-    swapped = ""
-    for i in range(len(codes)):
-        swapped += codes[i].swapcase()
-    return swapped
-
-def swap(string, pos1, pos2):
-    string = list(string)
-    string[pos1], string[pos2] = string[pos2], string[pos1]
-    string = ''.join(string)
-    return string
-
-def first_letter(string):
-    for i in range(len(string)):
-        if string[i].isalpha():
-            return i
-
-def last_letter(string):
-    for i in range(len(string)-1, -1, -1):
-        if string[i].isalpha():
-            return i
-
-def swap_letters(string):
-    pos1 = first_letter(string)
-    pos2 = last_letter(string)
-    string = swap(string, pos1, pos2)
-    return string
- 
-def del_highest(string):
-  char_list = list(string)
-  highest_number = -1
-  
-  for i, c in enumerate(char_list):
-    if c.isdigit():
-      if int(c) > highest_number:
-        highest_number = int(c)
-        highest_number_index = i
-  
-  del char_list[highest_number_index]
-  return ''.join(char_list)
-
-def del_lowest(string):
-  char_list = list(string)
-  lowest_number = float('inf')
-  
-  for i, c in enumerate(char_list):
-    if c.isdigit():
-      if int(c) < lowest_number:
-        lowest_number = int(c)
-        lowest_number_index = i
-  
-  del char_list[lowest_number_index]  
-  return ''.join(char_list)
 
 
 def reload():
